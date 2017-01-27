e3b33ba3e197beadbede35a08dad800de3f858e7
jzhengli <lizhengriverside@gmail.com>
2017-01-27 10:15:11 -0500

    create folder for xml file if not exists

--- CCTV_sync.py
+++ CCTV_sync.py
@@ -1,11 +1,15 @@
 ## This script is for updating CCTV request. Changes will be synced from transactional to CCTV.gdb on shared drive. 
-import arcpy
+import arcpy, os
 from arcpy import env
 
-
+#create following path in C drive is not exists to store schema xml files
+xmlPath = "C:/ReplicaSchema"
+if not os.path.exists(xmlPath):
+	os.makedirs(xmlPath)
+env.workspace = xmlPath
 env.overwriteOutput = True
+
 # env.workspace = "Database Connections/RPUD_TRANSDB.sde"
-env.workspace = "C:/data"
 
 ## set up variables
 replica_gdb1 = "Database Connections/RPUD_TRANSDB.sde" #parent 
@@ -16,9 +20,11 @@ sync_direction = "FROM_GEODATABASE1_TO_2"
 ## compare replica schema
 print("Comparing replica schema...")
 arcpy.AddMessage("Comparing replica schema...")
-outputXML = "replicaSchema.xml"
+outputXML = "C:/data/replicaSchema.xml"
+# overwriteXML(outputXML)
 arcpy.ExportReplicaSchema_management(replica_gdb1, outputXML, replica_name)
-schemaChangeXML = "schemaChange.xml"
+schemaChangeXML = "C:/data/schemaChange.xml"
+# overwriteXML(schemaChangeXML)
 arcpy.CompareReplicaSchema_management(replica_gdb2, outputXML, schemaChangeXML)
 arcpy.ImportReplicaSchema_management(replica_gdb2, schemaChangeXML)
 
@@ -26,7 +32,8 @@ arcpy.ImportReplicaSchema_management(replica_gdb2, schemaChangeXML)
 print ("Updating CCTV data from {0} to {1}...".format(replica_gdb1, replica_gdb2))
 arcpy.AddMessage("Updating CCTV data from {0} to {1}...".format(replica_gdb1, replica_gdb2))
 
-## sync changes
+# sync changes
 arcpy.SynchronizeChanges_management(replica_gdb1, replica_name, replica_gdb2, sync_direction)
 print ("Sync complete.")
 arcpy.AddMessage("Sync complete.")
+
